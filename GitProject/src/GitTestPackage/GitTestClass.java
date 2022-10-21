f84b0584899a31f2d93d661ff98823544636527a
ThivakarDeivendran <dthivakar1@gmail.com>
2022-10-21 17:06:07 +0530

    Update the java file

--- GitProject/src/GitTestPackage/GitTestClass.java
+++ GitProject/src/GitTestPackage/GitTestClass.java
@@ -3,5 +3,8 @@ package GitTestPackage;
 public class GitTestClass {
 		public static void main(String[] args) {
 			System.out.println("Automation Test scrpits");
+			if("A"=="A") {
+				System.out.println("Print A");
+			}
 		}
 }
